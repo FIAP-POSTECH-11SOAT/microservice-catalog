7bc418ffbaa6602d2d7554f350c703275168f32d
Erick <erick.kuwahara79@gmail.com>
2025-12-06 20:10:23 -0300

    feat: add application health check and Swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { envSchema } from './infra/env/env'
+import { EnvModule } from './infra/env/env.module'
+import { HttpModule } from './infra/http/controllers/http.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+    HttpModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { cleanupOpenApiDoc } from 'nestjs-zod'
 import { AppModule } from './app.module'
+import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  })
+
+  // SWAGGER
+  const openAPIConfig = new DocumentBuilder()
+    .setTitle('FIAP POS TECH - Restaurant Management System')
+    .setDescription('Catalog Microsservice API documentation')
+    .setVersion('1.0')
+    .addTag('App', 'Endpoints related to application')
+    .addTag('Categories', 'Endpoints managing categories')
+    .addTag('Items', 'Manage product items and their details')
+    .build()
+  const openAPIDoc = SwaggerModule.createDocument(app, openAPIConfig)
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(openAPIDoc))
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+  await app.listen(port)
 }
 bootstrap()
